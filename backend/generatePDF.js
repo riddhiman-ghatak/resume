958ae2350848acb163305664116f7c15acaf61d5
Riddhiman Ghatak <riddhimanghatak@gmail.com>
2023-07-08 23:02:45 +0530

    major changes

--- backend/generatePDF.js
+++ backend/generatePDF.js
@@ -87,16 +87,97 @@
 // module.exports = { generatePDF };
 //..................................................................................................................................
 
+// const PDFServicesSdk = require('@adobe/pdfservices-node-sdk');
+// const fs = require('fs');
+// const { promisify } = require('util');
+
+// const OUTPUT = './generatedReceipt.pdf';
+// const template = './BasicTemplate1.docx';
+
+// function generatePDF(data) {
+//     return new Promise((resolve, reject) => {
+//         //if generatedReceipt already in directory remove it
+//         if (fs.existsSync(OUTPUT)) fs.unlinkSync(OUTPUT);
+
+//         if (fs.existsSync('./data.json')) {
+//             fs.unlinkSync('./data.json');
+//             console.log('Existing data.json file deleted.');
+//         }
+
+//         const credentials = PDFServicesSdk.Credentials
+//             .servicePrincipalCredentialsBuilder()
+//             .withClientId("37dddd52c7d94d72bf8d08b56b4b35a9")
+//             .withClientSecret("p8e-J_s77FaxPIUecousUL1euFv8V2MOnDnP")
+//             .build();
+
+//         const writeFileAsync = promisify(fs.writeFile);
+
+//         async function writeDataToFile(data) {
+//             const jsonString = JSON.stringify(data, null, 2);
+
+//             try {
+//                 await writeFileAsync('data.json', jsonString);
+//                 console.log('JSON file has been created!');
+//             } catch (err) {
+//                 console.error('Error writing JSON file:', err);
+//             }
+//         }
+
+//         async function executeDocumentMerge() {
+//             try {
+//                 await writeDataToFile(data);
+//                 const JSON_INPUT = require('./data.json');
+
+//                 const executionContext = PDFServicesSdk.ExecutionContext.create(credentials);
+
+//                 const documentMerge = PDFServicesSdk.DocumentMerge;
+//                 const documentMergeOptions = documentMerge.options;
+//                 const options = new documentMergeOptions.DocumentMergeOptions(
+//                     JSON_INPUT,
+//                     documentMergeOptions.OutputFormat.PDF
+//                 );
+
+//                 const documentMergeOperation = documentMerge.Operation.createNew(options);
+
+//                 const input = PDFServicesSdk.FileRef.createFromLocalFile(template);
+//                 documentMergeOperation.setInput(input);
+
+//                 documentMergeOperation.execute(executionContext)
+//                     .then(result => result.saveAsFile(OUTPUT))
+//                     .then(() => {
+//                         console.log('PDF generated successfully!');
+//                         resolve();
+//                     })
+//                     .catch(err => {
+//                         if (
+//                             err instanceof PDFServicesSdk.Error.ServiceApiError ||
+//                             err instanceof PDFServicesSdk.Error.ServiceUsageError
+//                         ) {
+//                             reject(`Exception encountered while executing operation: ${err}`);
+//                         } else {
+//                             reject(`Exception encountered while executing operation: ${err}`);
+//                         }
+//                     });
+//             } catch (err) {
+//                 console.error('Error during document merge:', err);
+//                 reject(err);
+//             }
+//         }
+
+//         executeDocumentMerge();
+//     });
+// }
+
+// module.exports = { generatePDF };
+//.......................................................................................................................................
 const PDFServicesSdk = require('@adobe/pdfservices-node-sdk');
 const fs = require('fs');
 const { promisify } = require('util');
 
 const OUTPUT = './generatedReceipt.pdf';
-const template = './BasicTemplate.docx';
 
 function generatePDF(data) {
     return new Promise((resolve, reject) => {
-
         if (fs.existsSync(OUTPUT)) fs.unlinkSync(OUTPUT);
 
         if (fs.existsSync('./data.json')) {
@@ -113,7 +194,10 @@ function generatePDF(data) {
         const writeFileAsync = promisify(fs.writeFile);
 
         async function writeDataToFile(data) {
-            const jsonString = JSON.stringify(data, null, 2);
+            // Remove the "resume_type" key from the data object
+            const { resume_type, ...dataWithoutResumeType } = data;
+
+            const jsonString = JSON.stringify(dataWithoutResumeType, null, 2);
 
             try {
                 await writeFileAsync('data.json', jsonString);
@@ -138,6 +222,14 @@ function generatePDF(data) {
                 );
 
                 const documentMergeOperation = documentMerge.Operation.createNew(options);
+
+                let template = './BasicTemplate1.docx'; // Default template
+
+                if (data.resume_type === "2") {
+                    template = './BasicTemplate2.docx';
+                } else if (data.resume_type === "3") {
+                    template = './BasicTemplate3.docx';
+                }
 
                 const input = PDFServicesSdk.FileRef.createFromLocalFile(template);
                 documentMergeOperation.setInput(input);
@@ -169,3 +261,4 @@ function generatePDF(data) {
 }
 
 module.exports = { generatePDF };
+
